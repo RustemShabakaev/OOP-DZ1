e645463c95c54789448aa6227d0ce95497915c67
rustem <prorusteg@gmail.com>
2023-08-30 21:06:17 +0300

    OOP dz1

---
+++ VendingMachine/src/Domen/HotDrink.java
@@ -0,0 +1,22 @@
+package Domen;
+
+public class HotDrink extends Bottle {
+    private int temperature;
+    public HotDrink (String name, int price, double bottleVolume, int temperature) {
+        super(name, price, bottleVolume);
+        this.temperature = temperature;
+
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+" temperature"+this.temperature;
+    }
+}
